448ccec9e9f3fb2985872d74593f2bda65c3c390
Darshan Vasani <vadanidarshan@gmail.com>
2025-07-19 14:05:39 +0530

    Parameters

--- routers/blog_post.py
+++ routers/blog_post.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, Path, Query
 from pydantic import BaseModel
 from typing import Optional
 
 router = APIRouter()
 
-# Pydantic schema
+# Pydantic model
 class BlogPost(BaseModel):
     title: str
     content: str
@@ -19,4 +19,15 @@ def create_blog(post: BlogPost = Body(...)):
     return {
         "message": "Blog created successfully!",
         "data": post
+    }
+
+@router.post("/new/{id}")
+def create_blogs(
+    id: int = Path(..., description="The ID of the blog post"),
+    version: int = Query(1, description="Version number of the post"),
+    blog: BlogPost = Body(...)
+):
+    return {
+        "message": f"Blog with ID {id} created with version {version}.",
+        "data": blog
     }
