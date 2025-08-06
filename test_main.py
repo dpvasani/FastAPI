636adcc05d166c035bf5243161882e7b4c95cba2
Darshan Vasani <vadanidarshan@gmail.com>
2025-08-06 12:33:45 +0530

    Model Guide And PyTest

---
+++ test_main.py
@@ -0,0 +1,8 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_get_all_blogs():
+    response = client.get("/blogs/all")
+    assert response.status_code == 200
