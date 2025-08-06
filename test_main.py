556c89b9d315f00bbd3bd416b251922073c392cc
Darshan Vasani <vadanidarshan@gmail.com>
2025-08-06 14:17:53 +0530

    Complete FastAPI Production Guide: Deployment, Debugging, Testing & Logging

--- test_main.py
+++ test_main.py
@@ -1,8 +1,11 @@
 from fastapi.testclient import TestClient
 from main import app
 
+print(TestClient.__module__)
+
+
 client = TestClient(app)
 
 def test_get_all_blogs():
-    response = client.get("/blogs/all")
+    response = client.get("/blog/all")
     assert response.status_code == 200
